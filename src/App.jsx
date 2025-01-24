ed2d3f3d46a471d707f5e71bdd96f4f01ea3721c
Abhayraj262 <abhayrajkashyap2002@gmail.com>
2025-01-24 20:25:08 +0530

    done sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/ui/Navbar'
+import { Sidebar } from 'lucide-react'
 
 const App = () => {
   return (
     <div className="bg-[#F6F8FC] w-screen h-screen overflow-hidden">
       <Navbar/>
+      <Sidebar/>
     </div> 
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { LuPencil } from "react-icons/lu";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='w-[15%]'>
+        <div className='flex items-center gap-2 bg-[#C2E7FF] p-4 rounded-2xl hover:shadow-md'>
+            <button>
+            <LuPencil size={"24px"} />
+            Compose
+            </button>
+
+        </div>
       
     </div>
   )
