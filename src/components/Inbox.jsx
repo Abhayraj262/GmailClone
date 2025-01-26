333439619bc3bf096206d649b915143c6bfe0250
Abhayraj262 <abhayrajkashyap2002@gmail.com>
2025-01-26 21:44:09 +0530

    new chages

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaCaretDown, FaUserFriends } from "react-icons/fa";
+import { IoMdMore, IoMdRefresh } from "react-icons/io";
+import { MdCropSquare, MdInbox } from "react-icons/md";
+import { GoTag } from "react-icons/go";
+
+const mailType = [
+  {
+    icon: <MdInbox size={"20px"} />,
+    text: "Primary",
+  },
+  {
+    icon: <GoTag size={"20px"} />,
+    text: "Promotions",
+  },
+  {
+    icon: <FaUserFriends size={"20px"} />,
+    text: "Social",
+  },
+];
 
 const Inbox = () => {
+  const [mailTypeSelected, setMailTypeSelected] = useState(0);
   return (
-    <div>
-      indox
+    <div className="flex-1 bg-white rounded-xl mx-5">
+      <div className="flex items-center justify-between px-4">
+        <div className="flex items-center gap-2 text-gray-700 py-2">
+          <div className="flex items-center gap-1">
+            <MdCropSquare size={"20px"} />
+            <FaCaretDown size={"20px"} />
+          </div>
+          <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+            <IoMdRefresh size={"20px"} />
+          </div>
+          <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+            <IoMdMore size={"20px"} />
+          </div>
+        </div>
+      </div>
+      <div className="h-[90vh] overflow-auto">
+        <div className="flex items-center gap-2">
+          {mailType.map((item, index) => (
+            <button
+              key={index}
+              className={`flex items-center gap-5 p-4 ${
+                mailTypeSelected === index
+                  ? "border-b-4 border-b-blue-600 text-blue-600"
+                  : "border-b-4 border-b-transparent"
+              } w-52 hover:bg-gray-100`}
+              onClick={() => {
+                setMailTypeSelected(index);
+              }}
+            >
+              {item.icon}
+              <span>{item.text}</span>
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inbox
+export default Inbox;
